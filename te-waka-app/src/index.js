3fa03c4b89245102ac7b2243ea18dc0ef608d271
nicholasob7 <nicholasob972@gmail.com>
2022-07-07 21:59:19 +1200

    cant recall 5

--- te-waka-app/src/index.js
+++ te-waka-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import Helper from './Helper';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,11 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
       <App />
-      {/* <Helper /> */}
   </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
