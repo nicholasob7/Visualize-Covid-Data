f19ad2f7addc131f9ce1df6fc644053b38422659
nicholasob7 <nicholasob972@gmail.com>
2022-07-11 11:46:44 +1200

    opened branch, compiling pass

--- te-waka-app/src/index.js
+++ te-waka-app/src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import SimpleExample from "./archive/SimpleExample";
-import ApiExample from "./archive/ApiExample";
+// import SimpleExample from "./archive/SimpleExample";
+// import ApiExample from "./archive/ApiExample";
 import ApiLineExample from "./archive/ApiLineExample";
 
 import reportWebVitals from "./archive/useless/reportWebVitals";
