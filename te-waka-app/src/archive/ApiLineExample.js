a40d3d8a1b9be1bdc322b7ea3e9facb45c02c331
Nick Do <linh.do@tyme.com>
2022-07-12 06:27:41 +0700

    working plot example

--- te-waka-app/src/archive/ApiLineExample.js
+++ te-waka-app/src/archive/ApiLineExample.js
@@ -11,7 +11,7 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
+} from "chart.js";
 
 ChartJS.register(
   CategoryScale,
@@ -27,86 +27,132 @@ export const options = {
   responsive: true,
   plugins: {
     legend: {
-      position: 'top',
+      position: "top",
     },
     title: {
       display: true,
-      text: 'Deaths vs Recovered in Confirmed Cases - Disease.sh',
-    }
+      text: "Deaths vs Recovered in Confirmed Cases - Disease.sh",
+    },
   },
   scales: {
-    xAxis: [{
-      type: 'time',
-      time: {
-        unit: 'day',
-      }
-    }],
-    yAxis: [{
-      type: [moment("20200320", "YYYYMMDD").fromNow()],
-    }]
-  }
+    xAxis: [
+      {
+        type: "time",
+        time: {
+          unit: "day",
+        },
+      },
+    ],
+  },
 };
 
-const labels = ['March 20', 'April 20', 'May 20', 'June 20', 
-'July 20', 'August 20', 'September 20', 'October 20', 'November 20', 'December 20', 'January 21'];
+const labels = [
+  "March 20",
+  "April 20",
+  "May 20",
+  "June 20",
+  "July 20",
+  "August 20",
+  "September 20",
+  "October 20",
+  "November 20",
+  "December 20",
+  "January 21",
+];
 // const labels = [moment("20200320", "YYYYMMDD").fromNow()];
 
 export const myData = {
   labels,
   datasets: [
     {
-      label: 'Cases',
+      label: "Cases",
       data: labels.map(() => 1),
-      borderColor: 'rgb(0, 0, 255)',
-      backgroundColor: 'rgba(0, 0, 255, 0.5)',
+      borderColor: "rgb(0, 0, 255)",
+      backgroundColor: "rgba(0, 0, 255, 0.5)",
     },
     {
-      label: 'Deaths',
+      label: "Deaths",
       data: labels.map(() => 2),
-      borderColor: 'rgb(255, 0, 0)',
-      backgroundColor: 'rgba(255, 0, 0, 0.5)',
+      borderColor: "rgb(255, 0, 0)",
+      backgroundColor: "rgba(255, 0, 0, 0.5)",
     },
     {
-      label: 'Recovered',
+      label: "Recovered",
       data: labels.map(() => 3),
-      borderColor: 'rgb(0, 255, 0)',
-      backgroundColor: 'rgba(107,142,35, 0.5)',
+      borderColor: "rgb(0, 255, 0)",
+      backgroundColor: "rgba(107,142,35, 0.5)",
     },
   ],
 };
 
 export default function ApiLineExample() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(myData);
 
   const transformDatetimeFormat = (input_date) => {
-    return moment(input_date, "M-D-YY").format("YYYY-MM-DD")
+    return moment(input_date, "M-D-YY").format("YYYY-MM-DD");
   };
 
   const transformDictIntoXYList = (input_dict) => {
-    let transformed_dict = {}
+    let transformed_dict = {};
     for (const [key, value] of Object.entries(input_dict)) {
       transformed_dict[transformDatetimeFormat(key)] = value;
     }
-    console.log(transformed_dict)
-    let output_list = []
+    // console.log(transformed_dict);
+    let output_list = [];
     for (const [key, value] of Object.entries(transformed_dict)) {
-      output_list.push({"x": key, "y": value})
+      output_list.push({ x: key, y: value });
     }
-    console.log(output_list)
-    return transformDictIntoXYList
-  }
+    // console.log(output_list);
+    return output_list;
+  };
+
+  const getPositionList = (input_xy_dict_list, key) => {
+    let output_x_list = [];
+    for (const item of input_xy_dict_list) {
+      output_x_list.push(item[key]);
+    }
+    // console.log(output_x_list);
+    return output_x_list;
+  };
 
   useEffect(() => {
-      api.getHistoricalData().then((response) => {
+    api.getHistoricalData().then((response) => {
       const data = response;
-      const cases = transformDictIntoXYList(data["cases"])
-      const deaths = transformDictIntoXYList(data["deaths"])
-      const recovered = transformDictIntoXYList(data["recovered"])
+      const cases = transformDictIntoXYList(data["cases"]);
+      const deaths = transformDictIntoXYList(data["deaths"]);
+      const recovered = transformDictIntoXYList(data["recovered"]);
+      const labels = getPositionList(cases, "x");
+      const input_data = {
+        labels: labels,
+        cases: getPositionList(cases, "y"),
+        deaths: getPositionList(deaths, "y"),
+        recovered: getPositionList(recovered, "y"),
+      };
       setData({
-        "cases": cases, "deaths": deaths, "recovered": recovered
-      })
+        labels: input_data.labels,
+        datasets: [
+          {
+            label: "Cases",
+            data: input_data.cases,
+            borderColor: "rgb(0, 0, 255)",
+            backgroundColor: "rgba(0, 0, 255, 0.5)",
+          },
+          {
+            label: "Deaths",
+            data: input_data.deaths,
+            borderColor: "rgb(255, 0, 0)",
+            backgroundColor: "rgba(255, 0, 0, 0.5)",
+          },
+          {
+            label: "Recovered",
+            data: input_data.recovered,
+            borderColor: "rgb(0, 255, 0)",
+            backgroundColor: "rgba(107,142,35, 0.5)",
+          },
+        ],
+      });
     });
-// eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
@@ -114,9 +160,8 @@ export default function ApiLineExample() {
       <h1>Te Waka</h1>
       <Line
         options={options}
-        data={myData}
+        data={data}
         // setData={setData}
-        
       />
     </div>
   );
