27953ad2ad1afe58a396e6215cc0fa973f10d7ae
nicholasob7 <nicholasob972@gmail.com>
2022-07-14 22:42:36 +1200

    doughnut continents

--- te-waka-app/src/index.js
+++ te-waka-app/src/index.js
@@ -6,6 +6,7 @@ import "./index.css";
 // import ApiExample from "./archive/ApiExample";
 import GlobalHistorical from "./Viz1/GlobalHistorical";
 import GlobalVaccineHistorical from "./Viz2/GlobalVaccineHistorical";
+import ContinentsDoughnut from "./Viz3/ContinentsDoughnut";
 // import NorthAmerica from "./Viz3/NorthAmerica";
 
 import reportWebVitals from "./archive/useless/reportWebVitals";
@@ -17,6 +18,7 @@ createRoot(rootElement).render(
     <ApiExample /> */}
     <GlobalHistorical />
     <GlobalVaccineHistorical />
+    <ContinentsDoughnut />
     {/* <NorthAmerica /> */}
 
   </>
