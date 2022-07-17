08eed80a083b3477dab93728fdbf32ccc7fd3fe1
sameera <samebeg@yahoo.com>
2022-07-18 01:12:51 +1200

    pie chart

--- te-waka-app/src/archive/pie.js
+++ te-waka-app/src/archive/pie.js
@@ -13,7 +13,7 @@ export default function PieChart() {
     api.getCountriesData().then((data) => setContries(data))
   }, []);
     //    console.log(contries);  
-       let Austerlia, NewZealand, Deathdata, Populationdata
+       let Austerlia, NewZealand, Deathdata, Populationdata, Activedata, Recovereddata
        let piediv = <div>sameera</div>
     for (const contry of contries) {
         if (contry.country =="Australia") {
@@ -67,11 +67,55 @@ export default function PieChart() {
                     },
               ],
                
-            }
+        }
 
             piediv = <div><Pie data={Deathdata}/>
             <Pie data={Populationdata}/></div>
+        
+      Recovereddata = {
+            labels: ['NewZealand', 'Austerlia'],
+          
+        
+            datasets: [
+              {
+                label : "Test per one Million",
+                data: [NewZealand.recovered,Austerlia.recovered],
+                    backgroundColor:[
+                      'red',
+                      'blue',
+                      'yellow'
+                    ]
+                },
+                
+              ],
+
+               
+        } 
+       Activedata = {
+                labels: ['NewZealand', 'Austerlia'],
+              
+            
+                datasets: [
+                  {
+                    label : "Test per one Million",
+                    data: [NewZealand.active,Austerlia.active],
+                        backgroundColor:[
+                          'red',
+                          'blue',
+                          'yellow'
+                        ]
+                    },
+                    
+                  ],
+                   
         }  
+        piediv = <div><Pie data={Deathdata}/>
+        <Pie data={Populationdata}/>
+        <Pie data={Activedata}/>
+        <Pie data={Recovereddata}/>
+        </div>
+       }       
+                
     return (
     <div className="App" style={{width:'30%', height:'30%'}}>
       <h1>Covid19 testing in Contries</h1>
