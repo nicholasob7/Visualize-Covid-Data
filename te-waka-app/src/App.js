2b2d38abda043cce0c24ed626a810a473a1a86bb
nicholasob7 <nicholasob972@gmail.com>
2022-07-27 01:39:35 +1200

    deleted junk files from branch

--- te-waka-app/src/App.js
+++ te-waka-app/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-// import { createRoot } from "react-dom/client";
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import "./index.css";
--- te-waka-app/src/index.js
+++ te-waka-app/src/index.js
@@ -1,42 +1,6 @@
-<<<<<<< HEAD
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import BarChart from "./archive/ApiBarChart";
-import PieChart from "./archive/pie";
-
-
-// import SimpleExample from "./archive/SimpleExample";
-// import ApiExample from "./archive/ApiExample";
-import ApiLineExample from "./archive/ApiLineExample";
-import PlotExample from "./archive/PlotExample";
-
-import reportWebVitals from "./archive/useless/reportWebVitals";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <>
-    {/* <SimpleExample />
-    <ApiExample /> */}
-    {/* <ApiLineExample />
-    <PlotExample /> */}
-    {/* <BarChart /> */}
-     <PieChart />  
-     
-
-  </>
->>>>>>> origin/Sam
-);
-
-// reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
