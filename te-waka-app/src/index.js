cf07cf3be757ae45a4fc34aba36f55f548e4f362
nicholasob7 <nicholasob972@gmail.com>
2022-07-27 01:16:58 +1200

    git anxiety

--- te-waka-app/src/index.js
+++ te-waka-app/src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -8,6 +9,34 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+=======
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import BarChart from "./archive/ApiBarChart";
+import PieChart from "./archive/pie";
+
+
+// import SimpleExample from "./archive/SimpleExample";
+// import ApiExample from "./archive/ApiExample";
+import ApiLineExample from "./archive/ApiLineExample";
+import PlotExample from "./archive/PlotExample";
+
+import reportWebVitals from "./archive/useless/reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    {/* <SimpleExample />
+    <ApiExample /> */}
+    {/* <ApiLineExample />
+    <PlotExample /> */}
+    {/* <BarChart /> */}
+     <PieChart />  
+     
+
+  </>
+>>>>>>> origin/Sam
 );
 
 // reportWebVitals();
