6dc8bb0f8c4e0fa68a78f9b686beb6f5bf7d7302
nicholasob7 <nicholasob972@gmail.com>
2022-08-03 02:05:53 +1200

    some charts displaying on route pages

--- te-waka-app/src/App.js
+++ te-waka-app/src/App.js
@@ -4,34 +4,43 @@ import {
     Route,
 } from "react-router-dom";
 import Home from './pages/home/Home';
-// import List from './pages/list/List';
 import Team from './pages/team/Team';
 import Instructors from './pages/instructors/Instructors';
+// import WorldCases from './pages/worldCases/WorldCases';
+// import WorldDeaths from './pages/worldDeaths/WorldDeaths';
+import { ContinentsDoughnut } from "./pages";
+import { ContinentsPolar } from "./pages";
+import { GlobalHistorical } from "./pages";
+import { GlobalVaccineHistorical } from "./pages";
+// import { BarChart } from "./pages/Barchart";
+// import { Pie } from "./pages/ozKiwiPie";
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
                 <Routes>
-                    <Route path="/">
-                        <Route index element={<Home />} />
-                        <Route path="Team">
-                            {/* <Route index element={<List />} /> */}
-                            <Route path="Team/:id" element={<Team />} />
-                            {/* <Route
-                                path="new"
-                                element={<New inputs={userInputs} title="Add New Team" />}
-                            /> */}
-                        </Route>
-                        <Route path="Instructors">
-                            {/* <Route index element={<List />} /> */}
-                            <Route path="Instructors/:id" element={<Instructors />} />
-                            {/* <Route
-                                path="new"
-                                element={<New inputs={userInputs} title="Add New Instructors" />}
-                            /> */}
-                        </Route>
-                    </Route>
+                    {/* Landing */}
+                    <Route path="/" element={(<Home />)} />
+                    <Route path="/home" element={(<Home />)} />
+
+                    {/* Team */}
+                    <Route path="/team" element={<Team />} />
+                    <Route path="/instructors" element={<Instructors />} />
+
+                    {/* Pages */}
+                    {/* <Route path="worldCases" element={<WorldCases />} />
+                    <Route path="worldDeaths" element={<WorldDeaths />} /> */}
+
+                    {/* Charts */}
+                    <Route path="continentsDoughnut" element={<ContinentsDoughnut />} />
+                    <Route path="continentsPolar" element={<ContinentsPolar />} />
+                    <Route path="globalHistorical" element={<GlobalHistorical />} />
+                    <Route path="globalVaccineHistorical" element={<GlobalVaccineHistorical />} />
+                    {/* <Route path="BarChart" element={<BarChart />} /> */}
+                    {/* <Route path="OzKiwiPie" element={<OzKiwiPie />} /> */}
+
+
                 </Routes>
             </BrowserRouter>
         </div>
--- te-waka-app/src/pages/Barchart/APIBarChart.jsx
+++ te-waka-app/src/pages/Barchart/APIBarChart.jsx
@@ -1,76 +1,94 @@
-import { useEffect, useState } from "react";
-import api from "../../api";
+import React from 'react'
 
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-} from "chart.js";
-import { Bar } from "react-chartjs-2";
-// import { isDOMComponent } from "react-dom/test-utils";
-
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-);
-
-
-
-export default function BarChart() {
-    const [continents, setContinents] = useState([]);
-
-
-    debugger
-
-    useEffect(() => {
-        api.getContinentsData()
-            .then((data) => setContinents(data))
-    }, []);
-
-
-    const labels = continents.map((label) => label.continent);
-
-    const data = {
-        labels: labels,
-        datasets: [
-            {
-                label: "Test per one Million",
-                data: continents.map((continent) => continent.testsPerOneMillion),
-                backgroundColor: "rgba(255,99,132,0.2)",
-                borderColor: "rgba(255,99,132,1)",
-                borderWidth: 2,
-                // Change color when hover
-                hoverBackgroundColor: "rgba(255,99,132,0.4)",
-                hoverBorderColor: "rgba(255,99,132,1)",
-            },
-
-            {
-                label: "Total Population",
-                data: continents.map((continent) => continent.population),
-                backgroundColor: "rgba(155,231,91,0.2)",
-                borderColor: "rgba(255,99,132,1)",
-                borderWidth: 2,
-                hoverBackgroundColor: "rgba(255,99,132,0.4)",
-                hoverBorderColor: "rgba(255,99,132,1)",
-            },
-
-        ]
-    };
-
+const APIBarChart = () => {
     return (
-        <div className="App">
-            <h1>Covid-19 testing in continents</h1>
-            <Bar data={data}
-            />
-
-        </div>
-    );
+        <div>APIBarChart</div>
+    )
 }
+
+export default APIBarChart
+
+
+
+
+
+
+
+
+
+// import { useEffect, useState } from "react";
+// import api from "../../api";
+
+// import {
+//     Chart as ChartJS,
+//     CategoryScale,
+//     LinearScale,
+//     BarElement,
+//     Title,
+//     Tooltip,
+//     Legend
+// } from "chart.js";
+// import { Bar } from "react-chartjs-2";
+// // import { isDOMComponent } from "react-dom/test-utils";
+
+// ChartJS.register(
+//     CategoryScale,
+//     LinearScale,
+//     BarElement,
+//     Title,
+//     Tooltip,
+//     Legend
+// );
+
+
+
+// export default function BarChart() {
+//     const [continents, setContinents] = useState([]);
+
+
+//     debugger
+
+//     useEffect(() => {
+//         api.getContinentsData()
+//             .then((data) => setContinents(data))
+//     }, []);
+
+
+//     const labels = continents.map((label) => label.continent);
+
+//     const data = {
+//         labels: labels,
+//         datasets: [
+//             {
+//                 label: "Test per one Million",
+//                 data: continents.map((continent) => continent.testsPerOneMillion),
+//                 backgroundColor: "rgba(255,99,132,0.2)",
+//                 borderColor: "rgba(255,99,132,1)",
+//                 borderWidth: 2,
+//                 // Change color when hover
+//                 hoverBackgroundColor: "rgba(255,99,132,0.4)",
+//                 hoverBorderColor: "rgba(255,99,132,1)",
+//             },
+
+//             {
+//                 label: "Total Population",
+//                 data: continents.map((continent) => continent.population),
+//                 backgroundColor: "rgba(155,231,91,0.2)",
+//                 borderColor: "rgba(255,99,132,1)",
+//                 borderWidth: 2,
+//                 hoverBackgroundColor: "rgba(255,99,132,0.4)",
+//                 hoverBorderColor: "rgba(255,99,132,1)",
+//             },
+
+//         ]
+//     };
+
+//     return (
+//         <div className="App">
+//             <h1>Covid-19 testing in continents</h1>
+//             <Bar data={data}
+//             />
+
+//         </div>
+//     );
+// }
